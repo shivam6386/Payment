7803411dbe561a8e371488b8048379632e90594b
shivam <shivammanitripathishiv@gmail.com>
2026-03-23 23:21:51 +0530

    server setup and db connection

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Import Express (CommonJS style)
 const express = require('express');
-
+const {createPayment,capturePayment} = require('./src/controllers/paymentController');
+const cors = require('cors');
+const mongoose = require("mongoose");
 
 const app = express();
+
+require('dotenv').config();
 
 
 app.use(express.json());
@@ -18,9 +22,23 @@ app.get('/api', (req, res) => {
 });
 
 
+app.use(cors({ origin: 'http://localhost:5173' }));
+
+
+app.use(express.json());
+
+app.post('/payment',createPayment);
+app.post("/capturePayment",capturePayment);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+
 const PORT = 3000;
 
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+console.log(process.env.TEST_KEY_ID);
